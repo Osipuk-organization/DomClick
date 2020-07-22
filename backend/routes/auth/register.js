bec8e6b4cdc073f33bac651d66de0358a24a43e6
Constantine <buzuev.k@mail.ru>
2020-07-22 22:14:49 +0400

    feat(migrate): migrate from old repo

---
+++ backend/routes/auth/register.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const asyncHandler = require("express-async-handler");
+const UserModel = require("../../models/user");
+const Errors = require("../../models/types/errors");
+
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { login } = req.body.params;
+    const user = await UserModel.findOne({ login });
+    if (user) {
+      throw Errors.authError.duplicateUser;
+    }
+    user = await UserModel.create(req.body);
+    const plainUser = JSON.parse(JSON.stringify(user));
+    delete plainUser.password;
+    res.json(plainUser);
+  })
+);
