7051bb03d01b85914e20e7fcedf4f9a214d1a224
SokolAlexander <asokol1994@gmail.com>
2020-08-04 20:18:52 +0300

    merged frontend-new

--- frontend/src/components/DCAttachements/index.jsx
+++ frontend/src/components/DCAttachements/index.jsx
@@ -1,28 +1,22 @@
-import React from 'react';
-import DCButton from '../DCButton';
+import React from "react";
+import DCButton from "../DCButton";
 
-import './styles.scss';
+import "./styles.scss";
 
 const DCAttachement = (props) => {
+  const { size } = props;
 
-	const {
-		size
-	} = props;
-	
-	const classes = {
-		'DCAttachement': true
-	};
-	
-	return (
-		<div className="DCAttachement">
-			<input id="file-input" type="file" name="file" multiple />
-			<DCButton
-				active
-				label="Выберите файл"
-			/>
-			<span>или перетащите в область</span>
-		</div>
-	)
+  const classes = {
+    DCAttachement: true,
+  };
+
+  return (
+    <div className="DCAttachement">
+      <input id="file-input" type="file" name="file" multiple />
+      <DCButton active label="Выберите файл" />
+      <span>или перетащите в область</span>
+    </div>
+  );
 };
 
 export default DCAttachement;
--- frontend/src/components/DCButtonGroup/index.jsx
+++ frontend/src/components/DCButtonGroup/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import DCButton from "../DCButton";
 
-import './style.scss';
+import "./style.scss";
 
 const DCButtonGroup = (props) => {
   const { buttons } = props;
