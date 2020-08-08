24823d077b57d94bcd174708ca534b499c6a18b8
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-08-08 11:59:50 +0400

    fix(permission): moved accessLevel to independent file

--- backend/middlewares/permission.js
+++ backend/middlewares/permission.js
@@ -2,18 +2,8 @@ const UserModel = require("../models/user");
 const GroupModel = require('../models/group');
 const DepartmentModel = require('../models/department')
 const UserTypes = reuire('../models/types/enumUserTypes')
-const UserTypeValues = Object.values(UserTypes)
 const Errors = require("../models/types/errors");
-const { EMPLOYEE } = require("../models/types/enumUserTypes");
-
-const AccessLevel = {
-    VISITOR: 0,
-    EMPLOYEE: 1,
-    "GROUP CHIEF": 2,
-    "DEPARTMENT CHIEF": 3,
-    "DIRECTOR": 4,
-    "ADMIN": 5
-}
+const AccessLevel = require ('../types/accessLevel')
 
 const getAccessLevel = async (user) =>{
     switch(user.role){
@@ -51,12 +41,12 @@ const getAccessLevel = async (user) =>{
 const hasPermission = async (req, res, permission,next) => {
     const access = await getAccessLevel(req.user)
     if (access.level === AccessLevel.ADMIN ||
-        access.level === AccessLevel.DIRECTOR ) next()
+        access.level === AccessLevel.DIRECTOR ) next(access)
     if (permission.level > access.level) throw Errors.authError.noPermission
     const haveUnit = access.units.find( (unit) => unit === permission.unit)
     const haveEmployee = access.employees.find( (employee) =>  employee === permission.employee)
-    if (!!permission.unit && haveUnit) next()
-    else if (!!permission.employee && haveEmployee) next()
+    if (!!permission.unit && haveUnit) next(access)
+    else if (!!permission.employee && haveEmployee) next(access)
     else throw Errors.authError.noPermission
 };
 
---
+++ backend/types/accessLevel.js
@@ -0,0 +1,8 @@
+module.exports = Object.freeze({
+    VISITOR: 0,
+    EMPLOYEE: 1,
+    "GROUP CHIEF": 2,
+    "DEPARTMENT CHIEF": 3,
+    "DIRECTOR": 4,
+    "ADMIN": 5
+})
