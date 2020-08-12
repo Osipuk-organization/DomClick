23e11b9e33f4191847b7adb45d522f43e0cb912b
Constantine <buzuev.k@mail.ru>
2020-08-12 22:49:09 +0400

    fix(router): add new routers to main router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,27 @@
-const router = require("express").Router();
-const flatsRouter = require("./flats");
-const usersRouter = require("./users");
-const authRouter = require("./auth");
-const errorMiddleware = require("../middlewares/error");
-const authenticateMiddleware = require("../middlewares/authentication");
+const router = require('express').Router()
+const authRouter = require('./auth')
+const departmentsRouter = require('./departments')
+const groupsRouter = require('./groups')
+const employeesRouter = require('./employees')
+const flatsRouter = require('./flats')
+const verifyRouter = require('./verify.js')
+const flatRouter = require('./flats.js') // to be deprecated
+const usersRouter = require('./users.js') // to be deprecated
+const errorMiddleware = require('../middlewares/error')
+const authenticateMiddleware = require('../middlewares/authentication')
 
-router.use("/auth", authRouter);
-router.use("/flats", flatsRouter);
-router.use(authenticateMiddleware);
-router.use("/users", usersRouter);
-router.use(errorMiddleware);
+router.use('/auth', authRouter)
+router.use('/flats', flatRouter) // to be deprecated
+router.use(authenticateMiddleware)
+router.use('/departments', departmentsRouter)
+router.use('/groups', groupsRouter)
+router.use('/employees', employeesRouter)
+router.use('/flats_new/', flatsRouter)
+router.use('/verify', verifyRouter)
+router.use('/users', usersRouter) // to be deprecated
+router.use(errorMiddleware)
 router.get('*', function (req, res) {
-    res.status(404).json({ "error": 404 });
-  });
+    res.status(404).json({ error: 404 })
+})
 
-module.exports = router;
+module.exports = router
