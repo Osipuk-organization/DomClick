b8af70e23c0544ab2653b6a63755308514749a80
Constantine <buzuev.k@mail.ru>
2020-08-13 22:50:26 +0400

    fix(signin): add status to json

--- backend/routes/auth/signin.js
+++ backend/routes/auth/signin.js
@@ -1,16 +1,16 @@
-const router = require("express").Router();
-const passport = require("../../middlewares/passport");
-const asyncHandler = require("express-async-handler");
-const cookieHandler = require("../../middlewares/cookieHandler");
+const router = require('express').Router()
+const passport = require('../../middlewares/passport')
+const asyncHandler = require('express-async-handler')
+const cookieHandler = require('../../middlewares/cookieHandler')
 
 router.post(
-  "/",
-  // Установка куки по "Запомнить меня!"
-  cookieHandler,
-  // Авторизация по паспорту
-  asyncHandler(passport.authenticate),
-  (req, res) => {
-    res.json(req.user);
-  }
-);
-module.exports = router;
+    '/',
+    // Установка куки по "Запомнить меня!"
+    cookieHandler,
+    // Авторизация по паспорту
+    asyncHandler(passport.authenticate),
+    (req, res) => {
+        res.status(200).json(req.user)
+    }
+)
+module.exports = router
