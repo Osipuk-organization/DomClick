4acb11c5067079995912cc95813d6a388db97f9a
SerSerch <serserch@yandex.ru>
2020-08-14 15:15:38 +0300

    fix attachment style

--- frontend/src/components/Attachment/Attachment.jsx
+++ frontend/src/components/Attachment/Attachment.jsx
@@ -7,7 +7,7 @@ export const Attachment = function(props) {
     <section className="attachment">
       <div className="_container _row attachment__container">
         <div className="_col-12 _col-lg-6 _p-n">
-          <h3 className="attachment__caption">Скачайте наше приложение</h3>
+          <h2 className="attachment__caption">Скачайте наше приложение</h2>
           <div className="attachment__text">Совершайте сделки безопасно, легко<br/>и&nbsp;комфортно на&nbsp;кончиках пальцев</div>
           <div className="attachment__get-box">
             <a className="link attachment__get _gpl" href="#"></a>
